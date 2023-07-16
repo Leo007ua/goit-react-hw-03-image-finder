fb8eb99f0794fbf3d61ccb02f6cdfc3f19ffe498
Leo007ua <leo.oliinyk@ukr.net>
2023-07-16 21:33:00 +0300

    add api and componentDidMount

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,42 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import { requestPhotos } from 'services/api';
+
+export class App extends Component {
+  state = {
+    pictureName: '',
+    responcedPhotos: [],
+    isLoading: false,
+    error: null,
+    page: 1,
+    totalPictures: null,
+    modal: { isOpen: false, modalData: null },
+  };
+
+  async componentDidMount(_, prevState) {
+    const { pictureName, page } = this.state;
+    if (
+      this.state.pictureName !== prevState.pictureName ??
+      this.state.page !== prevState.page
+    ) {
+      try {
+        this.setState({ isLoading: true });
+        const responcedPhotos = await requestPhotos(pictureName, page);
+        console.log(requestPhotos);
+        this.setState({
+          totalPictures: responcedPhotos.totalHits,
+          responcedPhotos:
+            page === 1
+              ? responcedPhotos.hits
+              : [...prevState.responcedPhotos, ...responcedPhotos.hits],
+        });
+      } catch (error) {
+        this.setState({ error: error.message });
+      } finally {
+        this.setState({ isLoading: false });
+      }
+    }
+  }
+  render() {
+    return <div>App</div>;
+  }
+}
